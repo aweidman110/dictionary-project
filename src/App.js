8822dcb15904a91e16e2ae103dc060c5b8fce44c
Aika Weidman <119600413+aweidman110@users.noreply.github.com>
2024-03-11 22:45:15 -0400

    Created the search engine

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import logo from "./shecodes-logo.png";
-import './App.css';
+import "./App.css";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo img-fluid" alt="logo"/>
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <img src={logo} className="App-logo img-fluid" alt="logo" />
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          <small>Coded by Aika Weidman</small>
+        </footer>
+      </div>
     </div>
   );
 }
-
-
